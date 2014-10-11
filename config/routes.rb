e209c517a53e7af411ad19fe5c37bd3309da59d4
rafael <liloo-lovliz27@yandex.ru>
2014-10-11 06:17:27 +0500

    Создал модель User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 P5::Application.routes.draw do
+
+  root to: "pages#home"
+  get "help" => "pages#help"
+  get "about" => "pages#about"
+  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
